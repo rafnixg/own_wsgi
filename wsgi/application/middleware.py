d881e9dfd31f226173aea7af34f22431815c09a7
Rafnix Guzmán <rafnixg@gmail.com>
2024-07-31 17:13:27 -0500

    chore: Refactor Middleware implementation

--- wsgi/application/middleware.py
+++ wsgi/application/middleware.py
@@ -2,16 +2,26 @@
 
 import time
 
-
-class TimingMiddleware(object):
+def timing_middleware(func):
     """Middleware to print the time taken for each request."""
-
-    def __init__(self, func):
-        self.func = func
-
-    def __call__(self, request):
+    def wrapper(request):
         start = time.time()
-        response = self.func(request)
+        response = func(request)
         end = time.time()
         print(f"Request took {end - start} seconds.")
         return response
+    return wrapper
+
+
+# class TimingMiddleware(object):
+#     """Middleware to print the time taken for each request."""
+
+#     def __init__(self, func):
+#         self.func = func
+
+#     def __call__(self, request):
+#         start = time.time()
+#         response = self.func(request)
+#         end = time.time()
+#         print(f"Request took {end - start} seconds.")
+#         return response
