af4c9a3014f13b66a74f006f10b40ee3ecfc5b9f
Rafnix Guzmán <rafnixg@gmail.com>
2025-03-12 11:53:23 -0500

    chore: Mejorar el script build_and_run.sh con mensajes de estado y manejo de errores

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # Build the Docker image
+echo "Building the Docker image..."
 docker build -t own_wsgi_image .
 
-# Check if a container with the same name already exists and remove it
-if [ "$(docker ps -aq -f name=own_wsgi)" ]; then
-    docker rm -f own_wsgi
+# exit if the build failed
+if [ $? -ne 0 ]; then
+    echo "Docker image build failed!"
+    exit 1
 fi
 
-# Run the Docker container
-docker run -p 8081:8081 --name own_wsgi own_wsgi_image
+echo "Docker image built successfully!"
+
